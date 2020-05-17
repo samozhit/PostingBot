75ae9362f5d3d25170d08019efc841eeff2ee81e
DvlprNkt-s <62923438+DvlprNkt-s@users.noreply.github.com>
2020-05-17 17:43:03 +0300

    first commit

--- PostingBot/bot.py
+++ PostingBot/bot.py
@@ -1,19 +1,28 @@
 import telebot
+from os import system
 
 ADMIN_TOKEN ='838760869:AAF4GjkiYdwZxLmWFlzEeHk0MqDfyD5w0_k'
 admin_bot = telebot.TeleBot(ADMIN_TOKEN)
 TELEGRAM_CHANNEL_ID = '@channelForTestOne'
 
 def post_on_tg( video,text=None ,audio=None, document=None, photo=None):
-        admin_bot.send_video(TELEGRAM_CHANNEL_ID, video)
+        admin_bot.send_document(TELEGRAM_CHANNEL_ID, video)
 
 
 
 
 @admin_bot.message_handler(content_types=['text', 'audio', 'document', 'photo', 'video'])
 def send_video(message):
+        pass
+
+admin_bot.send_message(462508160, 'updated successfully')
+
+@admin_bot.message_handler(commands=['update'])
+def update(message):
+    if message.chat.id == 462508160:
+        system("reboot")
 
 
-    post_on_tg(message.video,)
 
 admin_bot.polling()
+
